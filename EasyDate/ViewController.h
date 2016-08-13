3ada3f912e5a094cf3b396027e0c4440818bda48
Jordi Puigdellívol <jordi@gloobus.net>
2016-08-13 21:46:19 +0200

    Initial Commit

---
+++ EasyDate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EasyDate
+//
+//  Created by Jordi Puigdellívol on 13/8/16.
+//  Copyright © 2016 revo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EasyDate/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EasyDate
+//
+//  Created by Jordi Puigdellívol on 13/8/16.
+//  Copyright © 2016 revo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
